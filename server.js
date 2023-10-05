6cf9f404a31d61f429d843544b8d824bc4bb6a9c
radmirtask <65283041+radmirtask@users.noreply.github.com>
2023-10-04 21:46:39 -0400

    added new files

---
+++ server.js
@@ -0,0 +1,13 @@
+/*
+Assignment1 server.js
+Author: Radmir Taskynbayev	
+ID: 301239725 
+Date: 04/10/2023
+ */
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+var express = require('./config/express');
+var app = express();
+app.listen(3000);
+module.exports = app;
+console.log('Server running at http://localhost:3000/');
